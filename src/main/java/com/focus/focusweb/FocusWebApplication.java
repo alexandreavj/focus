02d6c725935ff0b83776e38ece679406df54d8b4
Alexandre Afonso Vaz Jacob <alexandrejacob@student.dei.uc.pt>
2026-08-22 15:44:26 +0100

    initial commit

---
+++ src/main/java/com/focus/focusweb/FocusWebApplication.java
@@ -0,0 +1,13 @@
+package com.focus.focusweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocusWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FocusWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/focus/focusweb/FocusWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.focus.focusweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocusWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
